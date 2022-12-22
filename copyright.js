d5b58f0876ba6f654d7d6dfc90012ace6c1b4eb4
Sharif Imtaz <48783765+sharifimtaz@users.noreply.github.com>
2022-12-22 20:27:54 +0600

    Add My website profile

--- copyright.js
+++ copyright.js
@@ -6,7 +6,7 @@ const copyrightsData = [
 function copyrightTemplate(copyright) {
     return `
     <div class="container">
-                <p class="copyright_text">Copyright 2022 All Right Reserved By.<a href="https://shantomondol.github.io/shanto/"> Shanto Mondol</p>
+                <p class="copyright_text">Copyright 2022 All Right Reserved By.<a href="https://shantomondol.github.io/shanto"> Shanto Mondol</a> &#38; <a href="https://sharifimtaz.github.io/"> Sharif Imtaz</a></p>
             </div>
     `;
 }

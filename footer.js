496a7f726b9fe32313ac70fc561e409c0cf10b76
Shanto <shantomondol816@gmail.com>
2022-12-22 16:56:37 +0600

    contact from change

--- footer.js
+++ footer.js
@@ -8,17 +8,17 @@ function footerTemplate(footer) {
     <div class="row">
                     <div class="col-lg-4 col-sm-12">
                         <div class="map_text">
-                            <a href="#"><img src="./images/map-icon.png" class="image_main"><span class="padding_left_10"> Ajman,Industrial Area-2 <br>Post Box: 9400, Office no: 404</span></a>
+                            <img src="./images/map-icon.png" class="image_main"><span class="padding_left_10"> Ajman,Industrial Area-2 <br>Post Box: 9400, Office no: 404</span>
                         </div>
                     </div>
                     <div class="col-lg-4 col-sm-12">
                         <div class="map_text">
-                            <a href="#"><img src="./images/call-icon.png" class="image_main"><span class="padding_left_10">(+97165377282)</span></a>
+                            <a href="tel:+97165377282"><img src="./images/call-icon.png" class="image_main"><span class="padding_left_10">(+97165377282)</span></a>
                         </div>
                     </div>
                     <div class="col-lg-4 col-sm-12">
                         <div class="map_text">
-                            <a href="#"><img src="./images/mail-icon.png" class="image_main"><span class="padding_left_10">rajibmondol060@gmail.com</span></a>
+                            <a href="mailto:parthoscrap@gmail.com"><img src="./images/mail-icon.png" class="image_main"><span class="padding_left_10">parthoscrap@gmail.com</span></a>
                         </div>
                     </div>
                 </div>
